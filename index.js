9c86b24754e694d3d96441c09db3d0b18dfc0538
Michał Hachuła <michal.hachula@gmail.com>
2022-05-26 23:42:12 +0200

    feat: add root index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  meta: {
+    type: "suggestion", // `problem`, `suggestion`, or `layout`
+    docs: {
+      description: "Provide a descriptive function name as an useEffect param.",
+      category: "Fill me in",
+      recommended: false,
+      url: null, // URL to the documentation page for this rule
+    },
+    fixable: "code", // Or `code` or `whitespace`
+    schema: [], // Add a schema if the rule has options
+  },
+  create: function (context) {
+    return {
+      ExpressionStatement(node) {
+        if (node.expression.arguments[0].type === "ArrowFunctionExpression") {
+          context.report({
+            node: node,
+            message: "Don't use anonymous functions in useEffect.",
+          });
+        }
+      },
+    };
+  },
+};
